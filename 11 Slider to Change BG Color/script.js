79f56f81a6a57c150b268f4faefeafce21a1bb7f
debug-node <anupraj.2085@gmail.com>
2025-11-19 21:47:22 +0530

    some change in slider to change  project

--- 11 Slider to Change BG Color/script.js
+++ 11 Slider to Change BG Color/script.js
@@ -5,28 +5,34 @@ const number = document.getElementById("rangeValue");
 
 
 function updateStyle() {
-
     const value = Number(number.innerText);
 
-    if (value >= 90) {
-        applyStyles('#000', "#f5d300");
-    } else if (value >= 70) {
-        applyStyles('#333', "#fe53bb");
-    } else if (value >= 50) {
-        applyStyles("#555", "#09fbd3");
-    } else if (value >= 25) {
-        applyStyles("#fff", "#08f7fe");
-    } else if (value === 0) {
+    if (value === 0) {
         applyStyles("red", "#333");
+    } else if (value < 25) {
+        applyStyles("#fff", "#08f7fe");
+    } else if (value < 50) {
+        applyStyles("#555", "#09fbd3");
+    } else if (value < 70) {
+        applyStyles("#333", "#fe53bb");
+    } else if (value < 90) {
+        applyStyles("#000", "#f5d300");
+    } else {
+        applyStyles("#000", "#f5d300");
     }
-
 }
 
 function applyStyles(color, backgroundColor) {
     number.style.color = color;
     body.style.backgroundColor = backgroundColor;
 
+    if (color === '#fff') {
+        number.style.textShadow = '0 6px 18px rgba(0,0,0,0.35)';
+    } else {
+        number.style.textShadow = 'none';
+    }
 }
+
 
 range.addEventListener("input", function () {
 
